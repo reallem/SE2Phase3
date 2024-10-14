e50bfc89cb5a087f0654ae53c2a17a4326ddd500
reallem <132096332+reallem@users.noreply.github.com>
2024-10-15 01:27:27 +0300

    Create TestMyNewSecCode.dart

---
+++ TestMyNewSecCode.dart
@@ -0,0 +1,9 @@
+// TestMyNewSecCode.dart
+import 'package:test/test.dart';
+import 'NewSecCode.dart';  // Import the code file to test
+
+void main() {
+  test('Add two numbers', () {
+    expect(add(2, 3), 5);  // Test case
+  });
+}
