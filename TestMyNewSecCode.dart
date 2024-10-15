c80c60739d4a6b07682d22041217fce7cb1deb08
reallem <132096332+reallem@users.noreply.github.com>
2024-10-15 20:41:01 +0300

    Delete TestMyNewSecCode.dart

--- TestMyNewSecCode.dart
+++
@@ -1,9 +0,0 @@
-// TestMyNewSecCode.dart
-import 'package:test/test.dart';
-import 'NewSecCode.dart';  // Import the code file to test
-
-void main() {
-  test('Add two numbers', () {
-    expect(add(2, 3), 5);  // Test case
-  });
-}
